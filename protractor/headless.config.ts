53c8649c89f16fefee7affe2ecffc048ce927e63
estivenLandazury <dvlopez9811>
2020-03-14 11:29:30 -0500

    Added Headless test

---
+++ protractor/headless.config.ts
@@ -0,0 +1,18 @@
+import { Config, browser } from 'protractor';
+import { reporter } from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  specs: [ '../test/google.spec.js' ],
+  SELENIUM_PROMISE_MANAGER: false,
+  onPrepare: () => {
+    reporter();
+    browser.ignoreSynchronization = true;
+  },
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  }
+};
